82962fd2ee1c0a328773a500eb1b655e27dd48a9
xueJL <1392269217@qq.com>
2022-05-30 16:37:15 +0800

    xueJLaddinit

---
+++ src/main/java/com/xuejl/cat/miaomiaomiao/initmiaomiaomiao.java
@@ -0,0 +1,11 @@
+package com.xuejl.cat.miaomiaomiao;
+
+import com.stx.domain.Paper;
+
+public class initmiaomiaomiao {
+  public static void main(String[] args) {
+    //
+      Paper paper = new Paper();
+      paper.setId(666);
+  }
+}
